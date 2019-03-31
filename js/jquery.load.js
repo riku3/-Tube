8eb6190c36b7a5b9cbec6b134cdd6e2c597f21c1
riku3 <39666818+riku3@users.noreply.github.com>
2019-03-31 11:39:52 +0900

    Update jquery.load.js

--- js/jquery.load.js
+++ js/jquery.load.js
@@ -1,23 +1,23 @@
-$(function() {
-    var h = $(window).height();
+jQuery(function() {
+    var h = jQuery(window).height();
     
-    $('.off-canvas-wrapper').css('display','none');
-    $('#loader-bg ,#loader').height(h).css('display','block');
+    jQuery('.off-canvas-wrapper').css('display','none');
+    jQuery('#loader-bg ,#loader').height(h).css('display','block');
 });
     
-$(window).load(function () { //全ての読み込みが完了したら実行
-    $('#loader-bg').delay(900).fadeOut(800);
-    $('#loader').delay(600).fadeOut(300);
-    $('.off-canvas-wrapper').css('display', 'block');
+jQuery(window).load(function () { //全ての読み込みが完了したら実行
+    jQuery('#loader-bg').delay(900).fadeOut(800);
+    jQuery('#loader').delay(600).fadeOut(300);
+    jQuery('.off-canvas-wrapper').css('display', 'block');
 });
     
 //10秒たったら強制的にロード画面を非表示
-$(function(){
+jQuery(function(){
     setTimeout('stopload()',10000);
 });
     
 function stopload(){
-    $('.off-canvas-wrapper').css('display','block');
-    $('#loader-bg').delay(900).fadeOut(800);
-    $('#loader').delay(600).fadeOut(300);
+    jQuery('.off-canvas-wrapper').css('display','block');
+    jQuery('#loader-bg').delay(900).fadeOut(800);
+    jQuery('#loader').delay(600).fadeOut(300);
 }
